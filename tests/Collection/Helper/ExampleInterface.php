1edd10e2cc4f57f374cdbcb668f46e67a0c0cd8b
Milos Jovanovic <email.yomy@gmail.com>
2019-01-11 14:10:22 +0100

    Update copyright

--- tests/Collection/Helper/ExampleInterface.php
+++ tests/Collection/Helper/ExampleInterface.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2018 Milos Jovanovic <email.yomy@gmail.com>
+ * Copyright 2018-2019 Milos Jovanovic <phplibs@yomy.work>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
